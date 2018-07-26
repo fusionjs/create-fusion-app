9f48e2636ca9da3ae32b21a51af2a2734a14d4b8
Jamie Domingo <jaybmw@aol.com>
2018-07-26 16:31:15 -0700

    styling updates

--- templates/basic/content/src/pages/home.js
+++ templates/basic/content/src/pages/home.js
@@ -4,10 +4,11 @@ import React from 'react';
 import {styled} from 'fusion-plugin-styletron-react';
 
 const Center = styled('div', {fontFamily: 'HelveticaNeue-Light, Arial', display: 'flex', alignItems: 'center', height: '100%' })
-const FusionStyle = styled('div', {fontSize: '80px', paddingLeft: '500px'})
-const GettingStartedLink = styled('a', {textDecoration: 'none', color: 'blue', fonSize: '25px', marginLeft: '15px', marginTop: '30px'});
+const FusionStyle = styled('div', {fontSize: '80px', color: '#4db5d9', paddingLeft: '500px', paddingRight: '30px'})
 const FullHeightDiv = styled('div', { height: '100%', backgroundColor: '#FFFFFF'});
-const Circle = styled('div', {height: '150px', width: '150px', backgroundColor: '#bbb', borderRadius: '50%'});
+const Circle = styled('div', {height: '180px', width: '180px', marginTop: '20px', backgroundColor: '#f0f8fa', ':hover': {backgroundColor: 'white'}, border: '10px solid #4db5d9',  borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'});
+// const InnerCircle = styled('div', {height: '150px', width: '150px', backgroundColor: 'white', ':hover': {backgroundColor: '#f0f8fa'}, border: '2px solid #4db5d9', borderRadius: '50%', display: 'flex', justifyContent: 'center', alignItems: 'center'});
+const GettingStartedLink = styled('a', {textDecoration: 'none', color: '#4db5d9', fontSize: '18px', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center', height: '100%'});
 
 const Home = () => (
     <FullHeightDiv>
@@ -20,15 +21,18 @@ const Home = () => (
         input::-webkit-inner-spin-button,input::-webkit-outer-spin-button,input::-webkit-search-cancel-button,input::-webkit-search-decoration,input::-webkit-search-results-button,input::-webkit-search-results-decoration{display:none;}
         `}
         </style>
-        <Center>
-           
+        <Center>  
+
         <FusionStyle>
             Fusion.js
         </FusionStyle>
-       
-       <Circle>
-        <GettingStartedLink href="https://fusionjs.com/docs/getting-started">Let's Get Started</GettingStartedLink>
-        </Circle>
+
+        <Center>
+            <Circle>
+                <GettingStartedLink href="https://fusionjs.com/docs/getting-started">Let's Get Started</GettingStartedLink>
+            </Circle>
+        </Center>
+
         </Center>
     </FullHeightDiv>
 );
